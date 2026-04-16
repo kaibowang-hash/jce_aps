903069a2c519e04a23063acf6d9ad8652c0406c5
kaibowang-hash <kaibo_wang@whjichen.cn>
2026-04-16 17:41:30 +0100

    Update setup.py

--- jce_aps/setup.py
+++ jce_aps/setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="jce_aps",
     version="0.0.1",
-    description="Injection molding APS app for ERPNext/Frappe",
-    author="OpenAI",
+    description="JCE Injection APS for ERPNext/Frappe",
+    author="KAIBO WANG",
+    author_email="",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    install_requires=[],
 )
